774cab17b04e140fa2365775b4b663ee4bf07827
cukrus <cukruss@gmail.com>
2016-11-14 22:08:24 +0200

    1 paskaitos kodas

---
+++ paskaituKodas/1paskaita/src/lt/vcs/HelloWorld.java
@@ -0,0 +1,44 @@
+package lt.vcs;
+
+/* sdfgsfdg
+sdgfsfdg
+*/
+
+/**
+ * klase HeelloWorld
+ * @author Cukrus
+ */
+public class HelloWorld {
+    
+    /**
+     * javadokas aprasantis musu kintamaji
+     */
+    public static String kintamas = "betkoks kintamasis";
+    
+    public String vardas;
+    
+    //konstruktoriai tarp kintamuju ir metodu deklaraciju
+
+    /**
+     * pagrindinis metodas
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Labas pasauli!");
+        HelloWorld objektas = new HelloWorld();
+        objektas.kintamas = "sfdsdf";
+        
+        if (true) {
+            String objektas2 = objektas.vardas;
+        }
+        
+        String obj3 = objektas2;
+        
+    }
+    
+    public void belekoksMetodas() {
+        objektas
+    }
+    
+}
+
---
+++ paskaituKodas/1paskaita/src/lt/vcs/NewClass.java
@@ -0,0 +1,15 @@
+package lt.vcs;
+
+/**
+ *
+ * @author Cukrus
+ */
+public class NewClass {
+    
+    public strictfp void metodas() {
+        String sdfasf = HelloWorld.kintamas;
+        HelloWorld hw = new HelloWorld();
+        sdfasf = hw.kintamas;
+    }
+    
+}
