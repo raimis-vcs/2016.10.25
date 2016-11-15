425ef429e1086cd9db740916ca66c96d54efafb7
cukrus <cukruss@gmail.com>
2016-11-15 21:38:43 +0200

    antros paskaitos kodas

---
+++ paskaituKodas/paskaita2/src/lt/vcs/AntraPaskaita.java
@@ -0,0 +1,37 @@
+package lt.vcs;
+
+/**
+ *
+ * @author Cukrus
+ */
+public class AntraPaskaita {
+    
+    static Float kas;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Antra paskaita");
+        System.out.print("234234");
+        System.out.print("sedfsdfgsdf");
+        
+        byte baitas = 77;                       Byte baitas2 = new Byte(baitas);
+        baitas = 88;
+        short trumpas = 7777;                   Short trumpas2 = new Short(trumpas);
+        int intas = 777777777;                  Integer intas2 = new Integer(777);
+        long longas = 7777777777777777777L;     Long longas2 = new Long(777L);
+        
+        float floatas = 123.555F;               Float floatas2 = new Float(floatas);
+        double doublas = 1234213.45645654645;   Double dbl = new Double(doublas);
+        
+        char charas = 'k';                      Character charas2 = new Character(charas);
+        
+        boolean boolas = true;                  Boolean boolas2 = new Boolean(true);
+    
+        int betkasp = 0;
+        
+        System.out.println(kas);
+    }
+    
+}
